9bd223216eab100b3a4c71ce5ebe2b5ee9114dba
Uzo Olisemeka <uzo@designbymobius.ca>
2017-06-15 08:28:28 -0400

    added header to viewport

--- webapp/js/behavior.js
+++ webapp/js/behavior.js
@@ -8,7 +8,7 @@ socket.on( 'new-upload', function ( file ){
 
   var upload_markup = '<div id="upload-' + uploads + '" class="upload">' +
                         '<img class="image" src="data:image/jpeg;base64,'+ file.content +'">' +
-                        '<div class="description">Source: ' + file.uploader + '</div>' +
+                        '<div class="description">Satellite: ' + file.uploader + '</div>' +
                       '</div>';
 
   viewport.innerHTML = upload_markup + viewport.innerHTML;
