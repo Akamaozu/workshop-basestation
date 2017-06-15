f8fdbf693b28d2e319802380f7dbc8d74e12ae2f
Uzo Olisemeka <uzo@designbymobius.ca>
2017-06-14 21:14:16 -0400

    added basic ui to view base station uploads

--- components/http-server.js
+++ components/http-server.js
@@ -2,15 +2,26 @@ var citizen = require('supe'),
     express = require('express')(),
     server = require('http').Server( express ),
     socketio = require('socket.io')( server ),
+    path = require('path'),
+    fs = require('fs'),
 
     connection_status = false;
 
 server.listen( process.env.PORT || 80 );
 
-express.get('/', function( req, res ){
+express.use( require('express').static( path.join( __dirname, '..', 'webapp' ) ) );
 
-  res.send( 'hello world' );
-});
+express.get('/', function(){
+
+  var markup_cache;
+
+  return function( req, res ){
+
+    if( !markup_cache ) markup_cache = fs.readFileSync( path.join( __dirname, '..', 'webapp', 'index.html' ), 'utf8' );
+    res.send( markup_cache );
+  };
+
+}());
 
 socketio.on( 'connection', function( socket ){
 
@@ -27,7 +38,9 @@ socketio.on( 'connection', function( socket ){
   });
 
   socket.on( 'file-upload', function( data ){
+
     console.log( 'action=upload satellite=' + name + ' file=' + data.name );
+    socketio.emit( 'new-upload', { uploader: name, content: new Buffer( file.content ).toString('base64') });
   });
 });
 
